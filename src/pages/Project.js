022b374b3494467c3002c5438f52301f53f61c3d
carlynos <carllynos12@hotmail.com>
2023-02-22 13:46:53 -0300

    função de atualização de projetos, e mensagem de atualização

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import Container from '../components/layout/Container';
 import LinkButton from '../components/layout/LinkButton';
 import Loading from '../components/layout/Loading';
+import Message from '../components/layout/Message';
+import ProjectForm from '../project/ProjectForm'
 import Style from './Project.module.css'
 
 export function Project() {
@@ -10,6 +12,9 @@ export function Project() {
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+
+    const [message, setMessage] = useState("teste");
+    const [messageType, setMessageType] = useState();
 
     useEffect(() => {
         fetch(`http://localhost:5000/projects/${id}`, {
@@ -27,11 +32,37 @@ export function Project() {
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
     }
+
+    function editPost(project){
+        //budget validation
+        if(project.budget < project.cost){
+            setMessage('Orçamento não pode ser menor que o valor alocado!')
+            setMessageType('error');
+            return false
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        }).then((resp) =>resp.json())
+        .then((data) => {
+            setProject(data)
+            setShowProjectForm(false)
+            setMessage('Projeto atualizado com sucesso!')
+            setMessageType('success');
+        })
+        .catch((err) => console.log(err))
+
+    }
   return (
     <>
     {project.name ? 
     <div className={Style.project_details}>
         <Container customClass="column">
+            {message ? <Message type={messageType} msg={message} /> : 'nada recebido'}
             <div className={Style.details_container}>
                 <h1>Projeto: {project.name}</h1>
                 <button className={Style.btn} onClick={toggleProjectForm}>{!showProjectForm ? 'Editar projeto' : 'Fechar'}</button>
@@ -48,7 +79,7 @@ export function Project() {
                         <span>Total Utilizado:</span> R$ {project.cost}
                     </p>
                 </div>
-            ) : (<div className={Style.project_info}>asd</div>)}
+            ) : (<div className={Style.project_info}><ProjectForm handleSubmit={editPost} textbtn="Concluir edição" projectData={project} /></div>)}
             </div>
         </Container>
     </div> : <Loading />}
